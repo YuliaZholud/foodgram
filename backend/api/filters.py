4c6aab3e862cadb3431419127365c289e0e6a9e5
Yulia Zholud <jolud.j@gmail.com>
2025-11-21 19:50:21 +0100

    Исправила 28.02

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -2,7 +2,7 @@
 
 from django_filters.rest_framework import FilterSet, filters
 
-from recipes.models import Ingredient, Recipe, Tag
+from recipes.models import Ingredient, Recipe
 
 
 class RecipeFilter(FilterSet):
@@ -14,21 +14,21 @@ class RecipeFilter(FilterSet):
     )
 
     def filter_is_favorited(self, queryset, name, value):
+        """Отфильтровать рецепты по наличию в избранном пользователя."""
         user = self.request.user
         if not value:
             return queryset
         if not user.is_authenticated:
             return queryset.none()
-        # ВАЖНО: используем правильный related_name
         return queryset.filter(favorites__user=user)
 
     def filter_is_in_shopping_cart(self, queryset, name, value):
+        """Отфильтровать рецепты по наличию в списке покупок пользователя."""
         user = self.request.user
         if not value:
             return queryset
         if not user.is_authenticated:
             return queryset.none()
-        # здесь, судя по ошибке, related_name уже правильный
         return queryset.filter(cart_recipes__user=user)
 
     class Meta:
@@ -36,27 +36,6 @@ class RecipeFilter(FilterSet):
 
         model = Recipe
         fields = ('author', 'tags', 'is_favorited', 'is_in_shopping_cart')
-
-    def _get_user(self):
-        """Вернуть текущего пользователя, если он аутентифицирован."""
-        user = getattr(self.request, 'user', None)
-        if user and user.is_authenticated:
-            return user
-        return None
-
-    def filter_is_favorited(self, queryset, name, value):
-        """Отфильтровать рецепты по наличию в избранном пользователя."""
-        user = self._get_user()
-        if value and user:
-            return queryset.filter(user_favorite__user=user)
-        return queryset
-
-    def filter_is_in_shopping_cart(self, queryset, name, value):
-        """Отфильтровать рецепты по наличию в списке покупок пользователя."""
-        user = self._get_user()
-        if value and user:
-            return queryset.filter(cart_recipes__user=user)
-        return queryset
 
 
 class IngredientFilter(FilterSet):
